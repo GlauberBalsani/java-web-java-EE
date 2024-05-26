16ba986d8e3010c292e585a5de77fcecf0b40867
GlauberBalsani <glauber_29@hotmail.com>
2024-05-26 13:52:41 -0300

    configurando a camada controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,35 +8,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/main"} )
+@WebServlet(urlPatterns = { "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	DAO dao = new DAO();
-	
 
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public Controller() {
+		super();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		dao.getConnection();
 	}
-	
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String action = request.getServletPath();
+		System.out.println(action);
 
-	
+		if (action.equals("/main")) {
+			getContatos(request, response);
+		}
+	}
+
+	protected void getContatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 
 }
--- src/main/webapp/agenda.jsp
+++ src/main/webapp/agenda.jsp
@@ -1,15 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 <meta charset="UTF-8">
 <title>Agenda de Contatos</title>
 <link rel="icon" href="imagens/favicon.png">
 <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-	<img src="imagens/agenda.png">
 	<h1>Agenda de Contatos</h1>
-	<a href="main" class="Botao1">Acessar</a>
-
+	
+	<a href="" class="Botao1">Novo Contato</a>
 </body>
 </html>
